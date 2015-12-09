d4b2a9b06251d2d4a900960a2dd4018ac7c0a0dc
Dennis Thinh Tan Nguyen <dttn@itu.dk>
2015-12-09 16:13:45 +0100

    Added Navigation on Pages

--- StudyConfigurationUI/StudyConfigurationUI/View/MainPage.xaml.cs
+++ StudyConfigurationUI/StudyConfigurationUI/View/MainPage.xaml.cs
@@ -12,6 +12,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using StudyConfigurationUI.View;
+using StudyConfigurationUI.View.Pages;
+using HomePage = StudyConfigurationUI.View.Pages.HomePage;
+using StudyCreationPage = StudyConfigurationUI.View.Pages.StudyCreationPage;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -30,6 +34,35 @@ namespace StudyConfigurationUI
         private void HamburgerButton_OnClick(object sender, RoutedEventArgs e)
         {
             SplitView.IsPaneOpen = !SplitView.IsPaneOpen;
+        }
+
+        /// <summary>
+        /// Naviagte between pages when a radio button is
+        /// pressed in splitview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RadioSplitPaneItemButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var pressedRadioButton = sender as RadioButton;
+            if (pressedRadioButton != null)
+            {
+                switch (pressedRadioButton.Tag.ToString())
+                {
+                    case "Home":
+                        MainFrame.Navigate(typeof(HomePage));
+                        break;
+                    case "StudyCreation":
+                        MainFrame.Navigate(typeof (StudyCreationPage));
+                        break;
+                    case "ManageStudies":
+                        MainFrame.Navigate(typeof (ManageStudiesPage));
+                        break;
+                    case "Resources":
+                        MainFrame.Navigate(typeof (ResourcePage));
+                        break;
+                }
+            }
         }
     }
 }
