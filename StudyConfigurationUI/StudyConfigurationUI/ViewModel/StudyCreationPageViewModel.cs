bf7bb49530a527d29f3f32e1ccf029142984cee3
Dennis Thinh Tan Nguyen <dttn@itu.dk>
2015-12-10 22:19:23 +0100

    Added missing submit button

---
+++ StudyConfigurationUI/StudyConfigurationUI/ViewModel/StudyCreationPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using StudyConfigurationUI.Annotations;
+using StudyConfigurationUI.Model;
+
+namespace StudyConfigurationUI.ViewModel
+{
+    public class StudyCreationPageViewModel : INotifyPropertyChanged
+    {
+        public StudyCreationPageViewModel()
+        {
+            Phases = new ObservableCollection<Phase>();
+        }
+
+        public ObservableCollection<Phase> Phases { get; private set; } 
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
