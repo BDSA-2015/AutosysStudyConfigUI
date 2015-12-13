ccaf538761bdc3c0d62d33fa88b93e14b65f9ebf
Dennis Thinh Tan Nguyen <dttn@itu.dk>
2015-12-13 15:10:18 +0100

    Cleaned up code and added PhaseCreationViewModel for later implementation

--- StudyConfigurationUI/StudyConfigurationUI/ViewModel/PhaseCreationViewModels/PhaseCreationViewModel.cs
+++ StudyConfigurationUI/StudyConfigurationUI/ViewModel/PhaseCreationViewModels/PhaseCreationViewModel.cs
@@ -5,17 +5,14 @@ namespace StudyConfigurationUI.ViewModel.PhaseCreationViewModels
 {
     public class PhaseCreationViewModel
     {
-        public ObservableCollection<Criteria> Criterias { get; }
-        public ObservableCollection<Task> Tasks{ get; }
-        public ObservableCollection<User> Users { get; }
-        public ObservableCollection<Datafield> Datafields { get; }
-
-
-
-
         public PhaseCreationViewModel()
         {
             Criterias = new ObservableCollection<Criteria>();
         }
+
+        public ObservableCollection<Criteria> Criterias { get; }
+        public ObservableCollection<Task> Tasks { get; }
+        public ObservableCollection<User> Users { get; }
+        public ObservableCollection<Datafield> Datafields { get; }
     }
 }
