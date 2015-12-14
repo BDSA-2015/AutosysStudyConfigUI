126ec0a630b5a24bc1431aa88a90bd8cd264445e
Dennis <dttn@itu.dk>
2015-12-14 10:53:42 +0100

    Added Team model object

--- StudyConfigurationUI/StudyConfigurationUI/Model/PhaseModels/Team.cs
+++ StudyConfigurationUI/StudyConfigurationUI/Model/PhaseModels/Team.cs
@@ -5,6 +5,8 @@ namespace StudyConfigurationUI.Model.PhaseModels
 {
     public class Team
     {
-         
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int[] Users { get; set; } 
     }
 }
