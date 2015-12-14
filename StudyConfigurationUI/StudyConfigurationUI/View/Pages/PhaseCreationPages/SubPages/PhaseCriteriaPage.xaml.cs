b3231c3c8a829429fe5d3b5cdb8dc1875fd1de30
Dennis <dttn@itu.dk>
2015-12-14 12:35:38 +0100

    Phasepage implemented

--- StudyConfigurationUI/StudyConfigurationUI/View/Pages/PhaseCreationPages/SubPages/PhaseCriteriaPage.xaml.cs
+++ StudyConfigurationUI/StudyConfigurationUI/View/Pages/PhaseCreationPages/SubPages/PhaseCriteriaPage.xaml.cs
@@ -26,5 +26,56 @@ namespace StudyConfigurationUI.View.Pages.PhaseCreationPages.SubPages
         {
             this.InitializeComponent();
         }
+
+        private void ExclusionCreationWindow_OnPrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            //Todo add creation of criteria and add it to obs list in viewmodel.
+            //throw new NotImplementedException();
+        }
+
+        private void ExclusionCreationWindow_OnOpened(ContentDialog sender, ContentDialogOpenedEventArgs args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void ExclusionComparator_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void ExclusionTagComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void InclusionCreationWindow_OnPrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void InclusionCreationWindow_OnOpened(ContentDialog sender, ContentDialogOpenedEventArgs args)
+        {
+            ///throw new NotImplementedException();
+        }
+
+        private void InclusionTagComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void InclusionComparator_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private async void AddInclusionBut_OnClick(object sender, RoutedEventArgs e)
+        {
+            await InclusionCreationWindow.ShowAsync();
+        }
+
+        private async void AddExclusionBut_OnClick(object sender, RoutedEventArgs e)
+        {
+            await ExclusionCreationWindow.ShowAsync();
+        }
     }
 }
