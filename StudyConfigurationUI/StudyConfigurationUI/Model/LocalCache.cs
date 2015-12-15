802227e2712e69fcc8b55b67f6a2ad3ec5b86916
Dennis <dttn@itu.dk>
2015-12-15 09:39:29 +0100

    Added Study and phase models. Working on LocalCacheModel

---
+++ StudyConfigurationUI/StudyConfigurationUI/Model/LocalCache.cs
@@ -0,0 +1,19 @@
+﻿// LocalCache.cs is a part of Autosys project in BDSA-2015. Created: 15, 12, 2015.
+// Creators: Dennis Thinh Tan Nguyen, William Diedricsehn Marstrand, Thor Valentin Aakjær Olesen Nielsen, 
+// Jacob Mullit Møiniche.
+
+using System.Collections.Generic;
+using StudyConfigurationUI.Model.PhaseModels;
+
+namespace StudyConfigurationUI.Model
+{
+    /// <summary>
+    /// Stores all retrieved data from server
+    /// </summary>
+    public class LocalCache
+    {
+         IList<Datafield> CachedDatafields { get; set; }
+        IList<User> CachedUsers { get; set; } 
+             
+    }
+}
