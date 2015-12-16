f57f6f06ad6c39cfef22178152638511ff5ff81b
Dennis <dttn@itu.dk>
2015-12-16 03:22:15 +0100

    Changed version number

--- StudyConfigurationUI/StudyConfigurationUI/ViewModel/MainPageViewModel.cs
+++ StudyConfigurationUI/StudyConfigurationUI/ViewModel/MainPageViewModel.cs
@@ -7,7 +7,7 @@ namespace StudyConfigurationUI.ViewModel
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
-        private double _version = 0.6;
+        private double _version = 0.7;
         public string VersionText => "Autosys Study Configuation - Version: " + _version;
         public event PropertyChangedEventHandler PropertyChanged;
 
