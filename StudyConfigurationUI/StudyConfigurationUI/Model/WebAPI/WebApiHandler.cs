400f267516ab0963756dbb7e1ab858421530a564
Dennis <dttn@itu.dk>
2015-12-16 04:57:57 +0100

    Add base methods for webAPI

--- StudyConfigurationUI/StudyConfigurationUI/Model/WebAPI/WebApiHandler.cs
+++ StudyConfigurationUI/StudyConfigurationUI/Model/WebAPI/WebApiHandler.cs
@@ -2,13 +2,49 @@
 // Creators: Dennis Thinh Tan Nguyen, William Diedricsehn Marstrand, Thor Valentin Aakjær Olesen Nielsen, 
 // Jacob Mullit Møiniche.
 
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using StudyConfigurationUI.Model.PhaseModels;
 using StudyConfigurationUI.Model.StudyModels;
 
 namespace StudyConfigurationUI.Model.WebAPI
 {
+    /// <summary>
+    /// This class is responsible for handling communication beween server and gui
+    /// </summary>
     public class WebApiHandler
     {
-        public void SendStudy(Study studyToSend)
+        /// <summary>
+        /// Sends a study to server
+        /// </summary>
+        /// <param name="studyToSend"></param>
+        /// <returns>Task</returns>
+        public async Task SendStudy(Study studyToSend)
+        {
+            var JSON = JsonConvert.SerializeObject(studyToSend);
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets all datafields from resource file
+        /// Server will parse the file and return the results
+        /// </summary>
+        /// <param name="resourceFile">File as string</param>
+        /// <returns>List of Datafields</returns>
+        public async Task<IList<Datafield>>GetDatafields(string resourceFile)
+        {
+            var JSON = JsonConvert.SerializeObject(resourceFile);
+           throw new System.NotImplementedException();
+
+        }
+
+        /// <summary>
+        /// Gets all stored user from database
+        /// </summary>
+        /// <returns>List of users</returns>
+        public Task<IList<User>> GetUsers()
         {
             throw new System.NotImplementedException();
         }
